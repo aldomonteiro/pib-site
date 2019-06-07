8623ca1de1669cf7a4a25e9595b6922d0c214ff3
Aldo Monteiro <aldomonteiro@gmail.com>
2019-06-07 18:29:29 -0300

    communicating with graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,20 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "BACKEND",
+        // This is field under which it's accessible
+        fieldName: "backend",
+        // Url to query from
+        url: "http://localhost:4001/",
+
+        // refetch interval in seconds
+        refetchInterval: 60,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,90 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const { backend: { categoryMany } } = data;
+  console.log(categoryMany);
+
+  const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2
+  })
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Container>
+        {/* Stack the columns on mobile by making one full-width and the other half-width */}
+        {categoryMany.map((category, index) => {
+          return category.flavors && category.flavors.length > 0 && (
+            <Row key={index}>
+              <Col md={12}>
+                <Row>
+                  <Col xs={12} md={12} sm={12}>
+                    <h2>{category.name}</h2>
+                  </Col>
+                </Row>
+                {category.flavors.map((flavor, index) =>
+                  <>
+                    <Row key={index}>
+                      <Col xs={12} md={6} sm={6}>
+                        <h4>{flavor.flavor}</h4>
+                      </Col>
+                      <Col xs={12} md={6} sm={6}>
+                        {category.pricings && category.pricings.length > 0
+                          ? category.pricings.map((pricing) => pricing.size.size + ' ' + formatter.format(pricing.price) + ' ')
+                          : formatter.format(flavor.price)}
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col xs={12} md={12} sm={12}>
+                        <p>{flavor.toppingsNames.map((topping) => topping.topping + ',')}</p>
+                      </Col>
+                    </Row>
+                  </>
+                )}
+              </Col>
+            </Row>
+          )
+        })}
+      </Container>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    backend {
+      categoryMany (filter:{pageId: "938611509676235"}) {
+        id
+        name
+        flavors {
+          id
+          flavor
+          price
+          toppings
+          toppingsNames {
+            topping
+          }
+        }
+        pricings {
+          size {
+            size
+            shortening
+          }
+          price
+        }
+      }
+    }
+  }
+`
